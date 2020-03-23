1e53d3333e23ef276e7d4328452e2542ccdb18df
mattman516 <mattman516@gmail.com>
2020-03-23 09:01:55 -0400

    add buttons to previous walks

--- app/containers/PreviousWalks/index.js
+++ app/containers/PreviousWalks/index.js
@@ -1,19 +1,33 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { makeSelectWalkList } from '../MapPage/selectors';
+import { FaUndo, FaTrash } from 'react-icons/fa';
 import moment from 'moment';
+import IconButton from '../../components/IconButton'
 
 export default function AddFollowers() {
   const walkList = useSelector(makeSelectWalkList());
   return (
     <React.Fragment>
-      <div style={{ position: 'absolute', zIndex: 3, top: 'calc(100vh - 100px)', height: 300, width: '100vw', backgroundColor: 'white', paddingTop: 20 }}>
+      <Wrapper>
+        {walkList.length !== 0 && (
+          <h3>Previous Walks</h3>
+        )}
         {walkList.map(walk => {
           return (
-            <div style={{ maxWidth: 1000, margin: 'auto' }}>
-              <h6>{walk.name}</h6>
-              <p>{getDate(walk.walkEnds)}</p>
-            </div>
+            <React.Fragment>
+              <div style={{ maxWidth: 1000, margin: 'auto', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <div>
+                  <h6>{walk.name}</h6>
+                  <p>{getDate(walk.walkEnds)}</p>
+                </div>
+                <div style={{ display: 'flex' }}>
+                  <IconButton><FaTrash/></IconButton>
+                  <IconButton><FaUndo/></IconButton>
+                </div>
+              </div>
+              <hr/>
+            </React.Fragment>
           )
         })}
         {walkList.length === 0 && (
@@ -21,10 +35,27 @@ export default function AddFollowers() {
             <p style={{ color: 'light-grey' }}>Click on the map to create your first walk!</p>
           </div>
         )}
-      </div>
+      </Wrapper>
     </React.Fragment>
   );
 }
 const getDate = (time) => {
   return moment.unix(time).format("MMM DD YYYY hh:mm a");
+}
+
+const Wrapper = props => {
+  return (
+    <div
+      {...props}
+      style={{
+        position: 'absolute',
+        padding: 20,
+        zIndex: 3,
+        top: 'calc(100vh - 100px)',
+        width: '100vw',
+        backgroundColor: 'white',
+        paddingTop: 20,
+      }}
+    />
+  )
 }
